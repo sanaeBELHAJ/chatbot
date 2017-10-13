b3210126ac5db8a524dd3756681b3c2ab1107f22
sanae <sanai-son112@hotmail.fr>
2017-10-13 23:10:11 +0200

    Tp alarme

--- app.js
+++ app.js
@@ -1,65 +1,148 @@
-var restify = require('restify');
-var botbuilder = require('botbuilder');
+const restify = require('restify');
+const botbuilder = require('botbuilder');
 
-//setuper restify server
-var server = restify.createServer();
+// setup restify server
+const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3987, function(){
-	console.log('%s bot started at %s', server.name, server.url);
+    console.log('%s bot started at %s', server.name, server.url);
 });
-//create chat connector
-var connector = new botbuilder.ChatConnector({
-	appId: process.env.APP_ID,
-	appPassword: process.env.APP_SECRET
+
+// create chat connector
+const connector = new botbuilder.ChatConnector({
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_SECRET
 });
-//Listning for user inputs
+
+// Listening for user input
 server.post('/api/messages', connector.listen());
+//menu 
+var myMenu = {
+    "Créer une nouvelle alarme" : {
+        "choice": "createAlarm"
+    },
+    "Consulter les alarmes actives" : {
+        "choice": "showAlarmActive"
+    },
+    "Consulter tout les alarmes": {
+        "choice": "showAlarmAll"
+    }
+}
 
-//Reply by echoing 
-var bot =  new botbuilder.UniversalBot(connector, function(session){
-//trouver une solution
-bot.on('typing',function(response){
-    session.send(response);
-    session.send(`TEST TEST`);
-});
+var bot = new botbuilder.UniversalBot(connector, [
+    function(session) {
+        session.beginDialog('myMenu');
+    }
+]);
 
+var alarms = [];
 
-bot.on('conversationUpdate', function(message){
-    if(message.membersAdded && message.membersAdded.length > 0){
-        var membersAdded = message.membersAdded
-            .map(function(x){
-            //Verifier si il s'agit pas d'un bot
-            if (x.id != message.address.bot.id) {
-                var membersAdded = x.name || ' ' + '(Id=' + x.id + ' )';
-                bot.send(new botbuilder.Message()
-                    .address(message.address)
-                    .text('Bienvenue   ' + membersAdded + '! il fait beau aujourdhui!')
-                 );      
+bot.dialog('myMenu', [
+    function(session) {
+        botbuilder.Prompts.choice(session, 'Menu : ', myMenu, {
+            listStyle: botbuilder.ListStyle["button"]
+        });    
+    },
+    function(session, results) {
+        session.beginDialog(myMenu[results.response.entity].choice);
+    } 
+]);
+
+bot.dialog('createAlarm', [
+    function(session, args, next) {
+        session.dialogData.alarm = args || {};
+        if(!session.dialogData.alarm.name) {
+            botbuilder.Prompts.text(session, "Quel est le nom de votre alarme?");
+        }else{
+            next();
+        }     
+    },
+    function(session, results, next) {
+        if(results.response) {
+            session.dialogData.alarm.name = results.response;
+        }
+        if(!session.dialogData.alarm.date) {
+            botbuilder.Prompts.time(session, "Quel date/heure?");
+        }else{
+            next();
+        }
+    },
+    function(session, results, next) {
+        if(results.response) {
+            session.dialogData.alarm.time = botbuilder.EntityRecognizer.resolveTime([results.response]);
+        }
+        if(!session.dialogData.alarm.active) {
+            botbuilder.Prompts.confirm(session, "Voulez-vous activer votre alarm?");
+        }
+    },
+    function(session, results, next) {
+        if(results.response) {
+            session.dialogData.alarm.active = results.response;
+        }
+        var alarm = {
+            "name" : session.dialogData.alarm.name,
+            "time" : session.dialogData.alarm.time,
+            "active" : session.dialogData.alarm.active
+        }
+        if(alarm.name && alarm.time) {
+            
+            alarms.push(alarm);
+            session.userData.alarms = alarms;
+
+            session.send('Alarme  <<' + alarm.name + '>> a bien été enregistré!');
+            session.replaceDialog('myMenu');
+        }else{
+            session.replaceDialog('create');
+        }
+    }
+])
+.reloadAction(
+    "restart", "",
+    {
+        matches: /^restart/i,
+        confirmPrompt: "êtes vous sûr de vouloir quitter cette alarme??"
+    }
+)
+.cancelAction(
+    "cancel", "Ops vous avez abondonner la création de l'alarme",
+    {
+        matches: /^cancel/i,
+        confirmPrompt: "êtes vous sur de vouloir annuler?"
+    }
+);
+
+//les alarmes active
+bot.dialog('showAlarmActive', [
+    function(session) {
+        var message = "Vos alarmes actives : <br/>"
+        if(session.userData.alarms) {
+            for(var alarm of session.userData.alarms) {
+                if(alarm.active) {
+                    message += "-  " + alarm.name + "      -  à: " + alarm.time + "    - status : active<br/>";
+                }
             }
-            }).join(', '); //sépare le tableau en string
-
-       
+            session.send(message);
+            session.replaceDialog('myMenu');
+        }else{
+            session.send(':( rien pour le moment');
+            session.replaceDialog('myMenu');
+        }
     }
-});
-//Bienvenu aux utilisateurs ajouté ou lors de nouvelle conversation ou quitter conversation
-bot.on('contactRelationUpdate', function(message){
-    if (message.action === 'add') {
-        var name = message.user ? message.user.name : null;
-        var reply = new botbuilder.Message()
-                .address(message.address)
-                .text("Bonjour %s... id : %s ", name , message.address.bot.id|| 'Comment ca va?');
-        bot.send(reply);
-    } else if(message.action === 'remove'){
-        var Id_Bot = message.address.bot.id;
-        var name = message.user ? message.user.name : null;
-        var reply = new botbuilder.Message()
-            .address(message.address)
-            .text(`${name} ${Id_Bot} a quitté la discution`);
-        bot.send(reply);
+]);
+//tout les alarmes
+bot.dialog('showAlarmAll', [
+    function(session) {
+        var message = "Tous les alarmes : <br/>"
+        if(session.userData.alarms) {
+            for(var alarm of session.userData.alarms) {
+                var isActivated = (alarm.active) ? "Active" : "Inactive";
+                message += "- Nom: " + alarm.name + "   -   à : " + alarm.time + "  -       Statut: " + isActivated + " <br/>";
+            }
+            session.send(message);
+            session.replaceDialog('myMenu');
+        }else{
+            session.send('Aucune alarme');
+            session.replaceDialog('myMenu');
+        }    
     }
-});
-
-
-});
-
-
+]);
 
